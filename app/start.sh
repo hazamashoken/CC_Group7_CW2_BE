674d35c5f6f589de5781e6076ead69c4a33b083c
tliangso <earth78203@gmail.com>
2023-12-08 05:27:55 +0000

    fix start cmd

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 python manage.py migrate \
-&& python manage.py createsuperuser --noinput \
-&& hypercorn app.asgi:application -b 0.0.0.0:8000
+&& python manage.py createsuperuser --noinput 
+
+hypercorn app.asgi:application -b 0.0.0.0:8000
