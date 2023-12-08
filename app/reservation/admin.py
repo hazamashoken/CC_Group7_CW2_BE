635e0dfeef6632b17afb51b805820bf5abe924c4
tliangso <earth78203@gmail.com>
2023-12-08 05:45:16 +0000

    add Model to admin site

--- app/reservation/admin.py
+++ app/reservation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from reservation.reservables.models import Reservable
+from reservation.reservation.models import Reservation
+
 # Register your models here.
+admin.site.register(Reservable)
+admin.site.register(Reservation)
